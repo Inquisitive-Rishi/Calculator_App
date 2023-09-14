58ec71d732dc8d6167f5d5d44a945c58f38cadc6
rishi_raj <rishiraj007bgp@gmail.com>
2023-09-14 23:01:38 +0530

    add basic function

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(a,b) {
+    return a+b;
+}
+
+function substract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+// 3 placeholder for a single operation. (a number, an operator, another number)
+let n1;
+let op;
+let n2;
+
+function operate(op,n1,n2) {
+    let result = (op == '+') ? add(n1,n2) :
+    (op == '-') ? substract(n1,n2) :
+    (op == '*') ? multiply(n1,n2) :
+    (op == '/') ? divide(n1,n2) :
+    console.log("Not a valid operator!");
+
+    return result;
+}
+
+console.log(operate('&',100,3));
