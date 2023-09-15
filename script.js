8b3c6d4c29bf1b4eb663e528f19c6d7606d66d6f
rishi_raj <rishiraj007bgp@gmail.com>
2023-09-15 23:27:34 +0530

    apply 3 variables for n1, operator and n2

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-let x = '';
+let n1 = '';
+let op = '';
+let n2 = '';
+let result;
 
 const display = document.querySelector('.screen');
 const dbBtn = document.querySelectorAll('#db');
-console.log(dbBtn);
+const eqlBtn = document.querySelector('.eql');
+// console.log(dbBtn);
+
 // buttons to be displayed on screen.
 dbBtn.forEach((btn) => {
     btn.addEventListener('click', () => {
         let txt = document.createElement('p');
         txt.textContent = btn.textContent;
         display.appendChild(txt);
-        // save to variables (only numbers and '.');
-        if (!(txt.textContent == Number(txt.textContent) || txt.textContent == '.')) {
-            return;
-        } else {
-            fillNum(txt.textContent);
-        }
-        console.log(x)
     });
 })
+
+// Add to variable x
+dbBtn.forEach((btn) => {
+    btn.addEventListener('click', ()=> {
+        if (!(btn.textContent == Number(btn.textContent) || btn.textContent == '.')) {
+            op+=btn.textContent;
+        } else {
+            if (!(op.length > 0)) {
+                addToN1(btn.textContent);
+            } else {
+                addToN2(btn.textContent);
+            }
+        }
+        console.log(n1)
+        console.log(op)
+        console.log(n2)
+    });
+});
 
 function add(a,b) {
     return a+b;
@@ -36,24 +52,29 @@ function divide(a,b) {
 }
 
 // 3 placeholder for a single operation. (a number, an operator, another number)
-let n1;
-let op;
-let n2;
 
 function operate(op,n1,n2) {
     let result = (op == '+') ? add(n1,n2) :
     (op == '-') ? substract(n1,n2) :
-    (op == '*') ? multiply(n1,n2) :
-    (op == '/') ? divide(n1,n2) :
+    (op == 'x') ? multiply(n1,n2) :
+    (op == '÷') ? divide(n1,n2) :
     console.log("Not a valid operator!");
 
     return result;
 }
 
-function fillNum(n) {
+function addToN1(n) {
     if (!(n == Number(n) || n == '.')) {
         return;
     } else {
-        x+=n;
+        n1+=n;
+    }
+}
+
+function addToN2(n) {
+    if (!(n == Number(n) || n == '.')) {
+        return;
+    } else {
+        n2+=n;
     }
 }
